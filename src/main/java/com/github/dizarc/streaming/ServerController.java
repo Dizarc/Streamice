ae08eb8d26c3452856cb76a575807ae9dd1c563e
faruk <faruk_yildiz@protonmail.com>
2024-05-04 20:57:44 +0300

    Initial commit

---
+++ src/main/java/com/github/dizarc/streaming/ServerController.java
@@ -0,0 +1,18 @@
+package com.github.dizarc.streaming;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ServerController {
+
+    @FXML
+    private Label videosCreatedLabel;
+    @FXML
+    private Label ClientLabel;
+
+    @FXML
+    private Label videoListView;
+    @FXML
+    private Label videoScrollBar;
+
+}
